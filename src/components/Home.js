e96a4d910b85b14cb4be8c29bb68d3b8a3cf794c
Aayush Biswas <mahuaaayush@gmail.com>
2021-10-03 11:30:01 +0530

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,8 @@ function Home() {
         <section id="home" className="text-white body-font">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-              <h1 className=" title title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Web And App
-                <br className="hidden lg:inline-block" />Development Classes
+              <h1 className=" title title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Web And App 
+                <br className="hidden lg:inline-block" /> Development Classes
               </h1>
               <p className="mb-8 leading-relaxed">Welcome to Web And App Development Classes. In this website you will be able to learn coding and create your own website or application.</p>
               <div className="flex justify-center">
